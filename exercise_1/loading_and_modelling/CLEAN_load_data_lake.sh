42b159af32f2e8a955f37bdf0706c6cedb215e0d
amkabatznick <amkabatznick@gmail.com>
2017-06-26 02:07:22 +0000

    Updated to reflect multiple directories

--- exercise_1/loading_and_modelling/CLEAN_load_data_lake.sh
+++ exercise_1/loading_and_modelling/CLEAN_load_data_lake.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #save my current directory
-MY_CWD = $(pwd)
+MY_CWD=$(pwd)
 
 #Clears staging directories
 rm ~/staging/excercise_1/*
@@ -9,13 +9,18 @@ rmdir ~/staging/excercise_1
 rmdir ~/staging
 
 #remove files from HDFS
-hdfs dfs -rm  /user/w205/hospital_compare/hospitals.csv 
-hdfs dfs -rm  /user/w205/hospital_compare/effective_care.csv
-hdfs dfs -rm  /user/w205/hospital_compare/readmissions.csv
-hdfs dfs -rm  /user/w205/hospital_compare/measures.csv
-hdfs dfs -rm  /user/w205/hospital_compare/surveys_responses.csv
+hdfs dfs -rm  /user/w205/hospital_compare/hospitals/hospitals.csv 
+hdfs dfs -rm  /user/w205/hospital_compare/effective_care/effective_care.csv
+hdfs dfs -rm  /user/w205/hospital_compare/readmissions/readmissions.csv
+hdfs dfs -rm  /user/w205/hospital_compare/measures/measures.csv
+hdfs dfs -rm  /user/w205/hospital_compare/surveys_responses/surveys_responses.csv
 
 #Remove HDFS directory
+hdfs dfs -rmdir  /user/w205/hospital_compare/hospitals/
+hdfs dfs -rmdir  /user/w205/hospital_compare/effective_care/
+hdfs dfs -rmdir  /user/w205/hospital_compare/readmissions/
+hdfs dfs -rmdir  /user/w205/hospital_compare/measures/
+hdfs dfs -rmdir  /user/w205/hospital_compare/surveys_responses/
 hdfs dfs -rmdir  /user/w205/hospital_compare/
 
 #Change Directory Back to Original
