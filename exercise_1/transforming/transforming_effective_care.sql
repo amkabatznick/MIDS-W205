dbcfc83668354f7a8702ba501018d6c234c49ae0
Andrew Kabatznick <amkabatznick@gmail.com>
2017-07-03 17:57:51 -0400

    Fixed effective care transformation

--- exercise_1/transforming/transforming_effective_care.sql
+++ exercise_1/transforming/transforming_effective_care.sql
@@ -19,4 +19,4 @@ Select
   cast(concat(substr(measure_start_date,7,4),'-',substr(measure_start_date,1,2),'-',substr(measure_start_date,4,2)) as DATE) measure_start_date,
   cast(concat(substr(measure_end_date,7,4),'-',substr(measure_end_date,1,2),'-',substr(measure_end_date,4,2)) as DATE) measure_end_date
 from effective_care
-where where cast(score as int) is not null;
+where cast(score as int) is not null;
