764713f30f6e28007d58100c7c8558e754a23514
Andrew Kabatznick <amkabatznick@gmail.com>
2017-08-06 17:07:09 -0400

    updated permissioning

--- exercise_2/Twittercredentials.py
+++ exercise_2/Twittercredentials.py
@@ -16,8 +16,8 @@ except:
     sys.stderr.write("TWITTER_ * environment variables not set \n")
     sys.exit(1)
 
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_secret)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
 
 
 
