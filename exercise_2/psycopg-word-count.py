dc372b4114651f8bba7699dbf061c160c4a3d7cb
Andrew Kabatznick <amkabatznick@gmail.com>
2017-08-06 20:28:49 -0400

    Added update and executre to psycopg-word-count.py

--- exercise_2/psycopg-word-count.py
+++ exercise_2/psycopg-word-count.py
@@ -12,3 +12,23 @@ print "word is", word
 
 conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
 cur = conn.cursor()
+
+cur.execute("UPDATE tweetwordcount SET count=count+1 WHERE word=%s", (uWord,))
+
+print "number of updated rows", cur.rowcount
+
+if cur.rowcount == 0:
+    cur.execute("INSERT INTO tweetwordcount (word,count) \
+          VALUES (%s, 1)",(word,))
+
+conn.commit()
+
+
+cur.execute("SELECT word, count from tweetwordcount")
+records = cur.fetchall()
+for rec in records:
+   print "word = ", rec[0]
+   print "count = ", rec[1], "\n"
+conn.commit()
+
+conn.close()
