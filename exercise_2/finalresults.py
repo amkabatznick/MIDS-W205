67ac184cd0a3ace977bf7e4e9aee3994dc0a693c
Andrew Kabatznick <amkabatznick@gmail.com>
2017-08-12 19:13:40 -0400

    Updated finalresults.py

--- exercise_2/finalresults.py
+++ exercise_2/finalresults.py
@@ -2,11 +2,22 @@ import sys
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
-conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
-cur = conn.cursor()
+if len(sys.argv) > 2:
+    print "Too Many Arguements Provided"
+    exit(1)
+else:
+    conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
+    cur = conn.cursor()
 
-if len(sys.argv) == 2:
-  word = sys.argv[1]
-  cur.execute("SELECT count from tweetwordcount WHERE word=%s", (word,))
-  #record =
-  print('Total number of occurrences of "',word,'": ',cur.fetchone()[0])
+    if len(sys.argv) == 1:
+        cur.execute("SELECT word, count from tweetwordcount")
+        records = cur.fetchall()
+        for rec in records:
+            print "(",rec[0],rec[1],")\n"
+    else: len(sys.argv) == 2:
+      word = sys.argv[1]
+      cur.execute("SELECT count from tweetwordcount WHERE word=%s", (word,))
+      print('Total number of occurrences of "',word,'": ',cur.fetchone()[0])
+
+    conn.commit()
+    conn.close()
