2307aff6e1fac356cdcd12dc460c167967bc46a1
Andrew Kabatznick <amkabatznick@gmail.com>
2017-08-12 13:30:32 -0400

    Added stop words

--- exercise_2/extweetwordcount/src/bolts/wordcount.py
+++ exercise_2/extweetwordcount/src/bolts/wordcount.py
@@ -3,15 +3,16 @@ from __future__ import absolute_import, print_function, unicode_literals
 from collections import Counter
 from streamparse.bolt import Bolt
 import psycopg2
+from ntlk.corpos import stopwords
 
-# to encode the string s.encode('utf8')
 
 
 class WordCounter(Bolt):
 
     def initialize(self, conf, ctx):
         self.counts = Counter()
-        self.
+        self.conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
+        self.stopWords = set(stopwords.words('english'))
 
     def process(self, tup):
         word = tup.values[0]
@@ -21,16 +22,15 @@ class WordCounter(Bolt):
         # Database name: Tcount
         # Table name: Tweetwordcount
         # you need to create both the database and the table in advance.
-        conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
-        cur = conn.cursor()
+        cur = self.conn.cursor()
+        if word not in self.stopWords:
+            if cur.rowcount == 0:
+                cur.execute("INSERT INTO tweetwordcount (word,count) \
+                      VALUES (%s, 1)",(word.encode('utf8'),))
+            else:
+                cur.execute("UPDATE tweetwordcount SET count=count+1 WHERE word=%s", (word.encode('utf8'),))
 
-        if cur.rowcount == 0:
-            cur.execute("INSERT INTO tweetwordcount (word,count) \
-                  VALUES (%s, 1)",(word.encode('utf8'),))
-        else:
-            cur.execute("UPDATE tweetwordcount SET count=count+1 WHERE word=%s", (word.encode('utf8'),))
-
-        conn.commit()
+        self.conn.commit()
 
         # Increment the local count
         self.counts[word] += 1
