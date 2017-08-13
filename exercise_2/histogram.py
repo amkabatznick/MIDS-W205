202adb63fb71eadff8ad3144fff314cecac2923c
Andrew Kabatznick <amkabatznick@gmail.com>
2017-08-13 08:30:34 -0400

    Updated histogram.py

--- exercise_2/histogram.py
+++ exercise_2/histogram.py
@@ -5,13 +5,14 @@ if len(sys.argv) < 2:
     print "Too Few Arguements Provided"
     exit(1)
 else:
-    print sys.argv[1]
+    words  = sys.argv[1].split(',')
+    print words
     # conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
     # cur = conn.cursor()
     #
     # count1 = sys.argv[1]
     # count2 = sys.argv[2]
-    # cur.execute("SELECT word, count from tweetwordcount where count between %s and %s order by count", (count1, count2))
+    # cur.execute("SELECT word, count from tweetwordcount where count between %s and %s order by count desc", (count1, count2))
     # records = cur.fetchall()
     # if not cur.rowcount:
     #     print "No Record Found in Range"
